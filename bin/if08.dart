5f4a61bacdc2a88a0ef55ee7ba58c49f0f38acc0
Пользователь <murodjontohirov2701@gmail.com>
2022-07-05 11:33:21 +0500

    if08

--- bin/if08.dart
+++ bin/if08.dart
@@ -11,35 +11,19 @@
     Returns:
         string: the message to print */
 
+//done
 String func(int a) {
-  if (a > 0) {
-    if (a > 0) (a % 2 == 0);
-    {
-      return "two-digit even number";
-    }
-  }
-  if (a < 0) {
-    if (a < 0) (a % 2 == 1);
-    {
-      return "two-digit odd number";
-    }
-  }
-  if (a < 0) {
-    if (a < 0) (a % 2 == 1);
-    {
-      return "three-digit odd number";
-    }
-  }
-  if (a < 0) {
-    if (a % 2 == 0) {
-      return "three-digit even number";
-    }
-  }
-  if (a == 0) {
-    if (a % 2 == 0) ;
+  if (a % 2 == 0 && a > 9 && a < 100) {
+    return "two-digit even number";
+  } else if (a % 2 == 1) {
+    return "two-digit odd number";
+  } else if (a % 2 == 1 && a > 99 && a < 1000) {
+    return "three-digit odd number";
+  } else if (a % 2 == 0) {
+    return "three-digit even number";
   }
 }
 
 void main() {
-  print(func(842));
+  print(func(82));
 }
